0fef4238ba7006200fa41cda4169f03cd29da6bd
jungheol <jungheol@naver.com>
2024-06-26 19:24:11 +0900

    model 클래스 생성

---
+++ src/main/java/com/example/zerobase/dividend/model/ScrapResult.java
@@ -0,0 +1,21 @@
+package com.example.zerobase.dividend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ScrapResult {
+
+    private Company company;
+
+    private List<Dividend> dividendList;
+
+    public ScrapResult() {
+        this.dividendList = new ArrayList<>();
+    }
+    
+}
