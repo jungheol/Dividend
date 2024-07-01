e9017c111b5643b24aa7d6926f99b92e5e621aee
jungheol <jungheol@naver.com>
2024-07-01 19:42:24 +0900

    회사 삭제 기능 구현

--- src/main/java/com/example/zerobase/dividend/web/CompanyController.java
+++ src/main/java/com/example/zerobase/dividend/web/CompanyController.java
@@ -1,9 +1,11 @@
 package com.example.zerobase.dividend.web;
 
 import com.example.zerobase.dividend.model.Company;
+import com.example.zerobase.dividend.model.constants.CacheKey;
 import com.example.zerobase.dividend.persist.entity.CompanyEntity;
 import com.example.zerobase.dividend.service.CompanyService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class CompanyController {
     private final CompanyService companyService;
+    private final CacheManager redisCacheManager;
 
     @GetMapping("/autocomplete")
     public ResponseEntity<?> autoComplete(@RequestParam String keyword) {
@@ -47,8 +50,16 @@ public class CompanyController {
         return ResponseEntity.ok(company);
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteCompany() {
-        return null;
+    @DeleteMapping("/{ticker}")
+    @PreAuthorize("hasRole('WRITE')")
+    public ResponseEntity<?> deleteCompany(@PathVariable String ticker) {
+        String companyName = this.companyService.deleteCompany(ticker);
+        // 캐시 삭제
+        this.clearFinanceCache(companyName);
+        return ResponseEntity.ok(companyName);
+    }
+
+    public void clearFinanceCache(String companyName) {
+        this.redisCacheManager.getCache(CacheKey.KEY_FINANCE).evict(companyName);
     }
 }
